bd3fcd4368ea6b6fec2f1db785f30f85768ffa79
Gracjan Górkiewicz <gracjano2002@wp.pl>
2022-11-13 16:47:46 +0100

    Zadanie 9 ale nie działa

---
+++ pp_zadania_1/Zadanie_9.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pp_zadania_1
+{
+    internal class Zadanie_9
+    {
+        int a;
+        public void PrintLiczbaParzysta()
+        {
+                GetNumberFromUser();
+                Console.WriteLine(LiczbaParzysta());
+        }
+        private void GetNumberFromUser()
+        {
+            Console.WriteLine("podaj liczbę a: ");
+            a = int.Parse(Console.ReadLine());
+            
+        }
+        private int LiczbaParzysta()
+        {
+             if (a % 2 == 0)
+            {
+                return Console.WriteLine($"Liczba {a} jest parzysta");
+            }
+              
+        } 
+     
+
+    }
+}
