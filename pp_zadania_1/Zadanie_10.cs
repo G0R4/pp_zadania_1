2cebeac2e04b8c4fa4a232ead476f0b76f828ebe
Gracjan Górkiewicz <gracjano2002@wp.pl>
2022-11-13 17:04:19 +0100

    Zadanie 10 również nie działające

---
+++ pp_zadania_1/Zadanie_10.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pp_zadania_1
+{
+    internal class Zadanie_10
+    {
+        int a;
+        public void PrintLiczbaParzystaNieparzysta()
+        {
+            GetNumberFromUser();
+            Console.WriteLine(LiczbaParzystaNieparzysta());
+        }
+        private void GetNumberFromUser()
+        {
+            Console.WriteLine("podaj liczbę a: ");
+            a = int.Parse(Console.ReadLine());
+
+        }
+        private int LiczbaParzystaNieparzysta()
+        {
+            if (a % 2 == 0)
+            {
+                return Console.WriteLine($"Liczba {a} jest parzysta");
+            }
+            else
+            {
+                return Console.WriteLine($"Liczba {a} jest nieparzysta");
+            }
+        }
+    }
+}
