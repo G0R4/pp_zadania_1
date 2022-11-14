1c6c3c7dd461f74ecfa0e78991c0b6a820125ebf
Gracjan Górkiewicz <gracjano2002@wp.pl>
2022-11-14 19:13:09 +0100

    zadanie 14

---
+++ pp_zadania_1/Zadanie_14.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pp_zadania_1
+{
+    internal class Zadanie_14
+    {
+        int a;
+
+
+
+        public void PrintCzyLiczbaJestDodatniaZeremUjemna()
+        {
+            GetNumberFromUser();
+            Console.WriteLine(CzyLiczbaJestDodatniaZeremUjemna());
+        }
+
+
+
+
+        private void GetNumberFromUser()
+        {
+            Console.WriteLine("podaj liczbę a: ");
+            a = int.Parse(Console.ReadLine());
+        }
+
+
+
+        private string CzyLiczbaJestDodatniaZeremUjemna()
+        {
+            if (a > 0)
+            {
+                return "1";
+            }
+            else if (a == 0)
+            {
+                return "0";
+            }
+            else
+            {
+                return "-1";
+            }
+        }
+    }
+}
