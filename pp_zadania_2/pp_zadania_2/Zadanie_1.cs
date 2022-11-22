2bcaefd7d0f8f67c8bbb19c390374a17a533fd7c
Gracjan Górkiewicz <gracjano2002@wp.pl>
2022-11-22 13:28:22 +0100

    Zestaw2_Zadanie1

--- pp_zadania_2/pp_zadania_2/Zadanie_1.cs
+++ pp_zadania_2/pp_zadania_2/Zadanie_1.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,16 +8,9 @@ namespace pp_zadania_2
 {
     internal class Zadanie_1
     {
-        int[] liczby = new int[10];
-        int i;
-
         public void PrintLiczbyWOdwrotnejKolejnosci()
         {
-            GetNumbersFromUser();
-            Console.WriteLine(OdwrocKolejnoscLiczb());
-        }
-        private void GetNumbersFromUser()
-        {
+            int[] liczby = new int[10];
             Console.WriteLine("Podaj 10 liczb:");
             for (int i = 0; i < liczby.Length; i++)
             {
@@ -27,13 +19,6 @@ namespace pp_zadania_2
             for (int i = liczby.Length - 1; i >= 0; i--)
             {
                 Console.WriteLine(liczby[i]);
-            }
-
-        }
-        private int OdwrocKolejnoscLiczb()
-        {           
-            {
-                return (" ");
             }
 
         }
